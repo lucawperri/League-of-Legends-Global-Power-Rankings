838c1807e35ad32ab6f73ae53bc7160ccf606263
lucawperri <lucawperri@gmail.com>
2023-10-04 12:15:11 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TournamentComponent } from './tournament/tournament.component';
+import { GlobalComponent } from './global/global.component';
+import { TeamComponent } from './team/team.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tournament', component: TournamentComponent },
+  { path: 'global', component: GlobalComponent },
+  { path: 'team', component: TeamComponent },
+  { path: '', redirectTo: '/tournament', pathMatch: 'full' }, // Default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TournamentComponent } from './tournament/tournament.component';
+import { GlobalComponent } from './global/global.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TournamentComponent,
+    GlobalComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
